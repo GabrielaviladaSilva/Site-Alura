811abfd57a6fa9ad00e4d50a3353f916d39bd4df
GabrielaviladaSilva <avila.silva.gabriel@escola.pr.gov.br>
2026-07-07 07:54:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// ===============================
+// CURIOSIDADE 1
+// ===============================
+
+function mostrarMensagem1() {
+
+    let texto = document.getElementById("texto");
+
+    texto.innerHTML =
+        "🦜 A América do Sul possui a maior biodiversidade do planeta. Só o Brasil abriga mais de 120 mil espécies de animais conhecidas.";
+
+    texto.style.color = "#FFD166";
+
+}
+
+
+// ===============================
+// CURIOSIDADE 2
+// ===============================
+
+function mostrarMensagem2() {
+
+    let texto = document.getElementById("texto");
+
+    texto.innerHTML =
+        "🌳 A Floresta Amazônica é a maior floresta tropical do mundo e abriga cerca de 10% de todas as espécies conhecidas da Terra.";
+
+    texto.style.color = "#90EE90";
+
+}
